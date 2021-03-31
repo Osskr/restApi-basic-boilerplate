2ce9dcb9c54787bc74b96ce1a2fc88345fa5b2d2
Osskr <osskrjuarez@gmail.com>
2021-03-30 21:50:19 -0300

    example.env added

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -10,7 +10,7 @@ const esRoleValido = async (rol='') => {
     const existeRol = await Role.findOne({ rol })
     if(!existeRol){
         throw new Error(`El rol ${ rol } no esta registrado en la Base de datos`)
-    }
+    } 
 }
 
 
